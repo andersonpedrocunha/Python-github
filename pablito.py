15c1b521d976cf68f0076c17588c88c2354c006b
Anderson Cunha <anderson.pedro01@gmail.com>
2020-05-10 15:57:32 +0000

    V1.2 pablito invertido

--- pablito.py
+++ pablito.py
@@ -4,10 +4,9 @@ print(resposta)
 idade = input('Pablo, qual a sua idade?')
 
 idade = idade.strip()
-if not idade.isnumeric():
-	print('errou feio, errou rude')
+if idade.isnumeric():
+    ano_que_vem = idade + 1
+	print('minha idade ano que vem é', ano_que_vem)
 	
 else:
-	ano_que_vem = int(idade) + 1
-  
-	print('minha idade ano que vem é', ano_que_vem)
+    print('Errou feio, errou rude.')
